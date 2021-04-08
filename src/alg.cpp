c54cbba7e2b051265dc46015ec52360e4cd4f783
temamudrov <79166955+temamudrov@users.noreply.github.com>
2021-04-08 15:16:19 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,31 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  int right = size;
+  int left = 0;
+  int count = 0;
+  int average;
+  while (left < right) {
+    average = (left + right) / 2;
+  if (arr [mid] == value) {
+    for (int i = average; arr [i] == value; i++) {
+      if (arr [average] == value) {
+        count++;
+      }
+    }
+    for (int j = average - 1; arr[j] == value; j--) {
+      if (arr [average] == value) {
+        count++;
+      }
+    }
+    break;
+  }
+    else if (arr [average] > value) {
+      right = average;
+    }
+    else {
+      left = average + 1;
+    }
+  }
+  return count;
 }
