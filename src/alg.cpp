fc6f0abe86bd87d507a4793b373ba196eb459ef1
temamudrov <79166955+temamudrov@users.noreply.github.com>
2021-04-12 13:18:14 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,27 +4,27 @@
   int left = 0;
   int count = 0;
   int average;
-  while (left < right) {
-    average = (left + right) / 2;
-  if (arr [mid] == value) {
-    for (int i = average; arr [i] == value; i++) {
-      if (arr [average] == value) {
-        count++;
+    while (left < right) {
+      average = (left + right) / 2;
+      if (arr [mid] == value) {
+        for (int i = average; arr [i] == value; i++) {
+          if (arr [average] == value) {
+            count++;
+          }
+        }
+        for (int j = average - 1; arr[j] == value; j--) {
+          if (arr [average] == value) {
+            count++;
+          }
+        }
+        break;
+      }
+      else if (arr [average] > value) {
+        right = average;
+      }
+      else {
+        left = average + 1;
       }
     }
-    for (int j = average - 1; arr[j] == value; j--) {
-      if (arr [average] == value) {
-        count++;
-      }
-    }
-    break;
+    return count;
   }
-    else if (arr [average] > value) {
-      right = average;
-    }
-    else {
-      left = average + 1;
-    }
-  }
-  return count;
-}
