aa6be9fa12d2807c925340e520311c2283c9b344
temamudrov <79166955+temamudrov@users.noreply.github.com>
2021-04-13 11:50:00 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,30 +1,30 @@
 // Copyright 2021 NNTU-CS
-  int cbinsearch(int *arr, int size, int value) {
-  int right = size;
-  int left = 0;
-  int count = 0;
-  int average;
-    while (left < right) {
-      average = (left + right) / 2;
-      if (arr [mid] == value) {
-        for (int i = average; arr [i] == value; i++) {
-          if (arr [average] == value) {
-            count++;
-          }
+int cbinsearch(int *arr, int size, int value) {
+int right = size;
+int left = 0;
+int count = 0;
+int average;
+  while (left < right) {
+    average = (left + right) / 2;
+    if (arr [mid] == value) {
+      for (int i = average; arr [i] == value; i++) {
+        if (arr [average] == value) {
+          count++;
         }
-        for (int j = average - 1; arr[j] == value; j--) {
-          if (arr [average] == value) {
-            count++;
-          }
+      }
+      for (int j = average - 1; arr[j] == value; j--) {
+        if (arr [average] == value) {
+          count++;
         }
-        break;
       }
-      else if (arr [average] > value) {
-        right = average;
-      }
-      else {
-        left = average + 1;
-      }
+      break;
     }
-    return count;
+    else if (arr [average] > value) {
+      right = average;
+    }
+    else {
+      left = average + 1;
+    }
   }
+  return count;
+}
